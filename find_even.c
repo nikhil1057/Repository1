a01a6d5503f9bfefdf9cad61ea08942df75d753e
nikhil1057 <nikhiltiwari1057@gmail.com>
2018-01-21 17:15:05 +0530

    Add files via upload

---
+++ find_even.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int n,k,i;
+    scanf("%d%d",&n,&k);
+
+    for(i=n+1;i<k;i++)
+    {
+        if(i%2 == 0)
+            printf("%d\n",i);
+    }
+    return 0;
+}
